a06769df60dbc6c311db425f0ce3cac1e6d49bd9
Kyaw Thu <Kyaw Thu>
2016-03-01 16:59:52 -0800

    Kyaw modifications to expression stuff

--- src/lexer/expression/ExpressionNode.java
+++ src/lexer/expression/ExpressionNode.java
@@ -10,4 +10,32 @@ public class ExpressionNode {
     private ExpressionNode rightChild;
 
     private Token value;
+
+	public Token getValue() {
+		return value;
+	}
+
+	public void setValue(Token value) {
+		this.value = value;
+	}
+
+	public ExpressionNode getLeftChild() {
+		return leftChild;
+	}
+
+	public void setLeftChild(ExpressionNode leftChild) {
+		this.leftChild = leftChild;
+	}
+
+	public ExpressionNode getRightChild() {
+		return rightChild;
+	}
+
+	public void setRightChild(ExpressionNode rightChild) {
+		this.rightChild = rightChild;
+	}
+	
+	public boolean isLeaf() {
+		return leftChild == null && rightChild == null;
+	}
 }
