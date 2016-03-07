91cd32e1466a994eaae3377b8bddfba3080ebd8a
Kyle Housley <kyle.housley@atsid.com>
2016-03-07 14:12:00 -0800

    unused import

--- src/view/UI.java
+++ src/view/UI.java
@@ -8,7 +8,6 @@ import javax.swing.*;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.TableModel;
-import javax.swing.tree.ExpandVetoException;
 import java.awt.*;
 
 /**
